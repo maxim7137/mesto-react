9c226073a17bc7f50b863efc207026aacf986cde
maxim713 <maxim713@mail.ru>
2022-10-13 20:46:44 +0300

    feature: Вынесите компонент Card

---
+++ src/components/Card.js
@@ -0,0 +1,62 @@
+import trash from '../images/trash.svg';
+const srcError =
+  'https://camo.githubusercontent.com/2515d63ed9f010c45188fb16aa813f67c886fcb713f8395964abcbd22bd791ef/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f41394563427a64367438445a652f67697068792e676966';
+const buttonLikedStyle = {};
+// проверка своих карточек
+// лайкнута ли мной
+function isLikedByMe(array, userId) {
+  const arrayOfLikedId = [];
+  array.forEach((element) => {
+    arrayOfLikedId.push(element._id);
+  });
+  const containsUserId = arrayOfLikedId.some((element) => element === userId);
+  return containsUserId;
+}
+// моя ли карточка
+function isMyCard(userId, owner) {
+  return userId === owner._id;
+}
+// заменить картинку если ошибка
+function errorHandler(event) {
+  event.target.src = srcError;
+}
+
+function Card({ link, name, likes, userId, owner }) {
+  // owner._id | userId
+
+  return (
+    <li className="elements__element">
+      <img
+        src={link}
+        alt={name}
+        className="elements__image"
+        onError={errorHandler}
+      />
+      <div className="elements__captions">
+        <h2 className="elements__name">{name}</h2>
+        <div className="elements__like-container">
+          <button
+            className={
+              isLikedByMe(likes, userId)
+                ? 'elements__like-button elements__like-button_liked'
+                : 'elements__like-button'
+            }
+            type="button"
+            aria-label="отметить карточку"
+          ></button>
+          <span className="elements__like-counter">{likes.length}</span>
+        </div>
+      </div>
+      {isMyCard(userId, owner) && (
+        <img
+          src={trash}
+          alt="корзина"
+          className="elements__trash"
+          aria-label="удалить карточку"
+        />
+      )}
+    </li>
+  );
+}
+
+export default Card;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../utils/Api.js';
+import Card from './Card.js';
 
 import pen from '../images/pen.svg';
 import edit from '../images/edit.svg';
 import plus from '../images/plus.svg';
 
 function Main(props) {
+  const [userId, setUserId] = useState(null);
   const [userName, setUserName] = useState('Жак Ив Кусто');
   const [userDescription, setUserDescription] = useState(
     'Исследователь океана'
@@ -13,60 +15,34 @@ function Main(props) {
   const [userAvatar, setUseAvatar] = useState(
     'https://www.placecage.com/c/120/120'
   );
-  // Загрузка начальных данных --
-  api
-    .getInitialUser()
-    .then((result) => {
-      setUserName(result.name);
-      setUserDescription(result.about);
-      setUseAvatar(result.avatar)
-      const userId = result._id;
-/*
-      // Загрузка начальных карточек --
-      api
-        .getInitialCards()
-        .then((result) => {
-          cardsList.renderItems(result); // вставка карточек
-          const cardsNodeList = cardsContainer.querySelectorAll('li'); // вставка лайков
-          for (let i = 0; i < result.length; i++) {
-            cardsNodeList[i].querySelector('span').textContent =
-              result[i].likes.length;
-          }
-          // проверка своих карточек
-          // лайкнута ли мной
-          function isLiked(array) {
-            const arrayOfLikedId = [];
-            array.forEach((element) => {
-              arrayOfLikedId.push(element._id);
-            });
-            const containsUserId = arrayOfLikedId.some(
-              (element) => element === userId
-            );
-            return containsUserId;
-          } // лайкнута ли мной //
+  const [cards, setCards] = useState([]);
 
-          for (let i = 0; i < result.length; i++) {
-            // для удаления
-            if (result[i].owner._id !== userId) {
-              cardsNodeList[i].querySelector('.elements__trash').remove();
-            }
-            // для лайков
-            if (isLiked(result[i].likes)) {
-              cardsNodeList[i]
-                .querySelector(selectorsOfCard.buttonLike)
-                .classList.add(selectorsOfCard.liked);
-            }
-          }
-        })
-        .catch((err) => {
-          console.log(err); // выведем ошибку в консоль
-        });
-      // -- Загрузка начальных карточек // */
-    })
-    .catch((err) => {
-      console.log(err); // выведем ошибку в консоль
-    });
-  // -- Загрузка начальных данных //
+  useEffect(() => {
+    // Загрузка начальных данных --
+    api
+      .getInitialUser()
+      .then((result) => {
+        setUserName(result.name);
+        setUserDescription(result.about);
+        setUseAvatar(result.avatar);
+        setUserId(result._id);
+        // Загрузка начальных карточек --
+        api
+          .getInitialCards()
+          .then((result) => {
+            setCards(result);
+          })
+          .catch((err) => {
+            console.log(err); // выведем ошибку в консоль
+          });
+        // -- Загрузка начальных карточек //
+      })
+      .catch((err) => {
+        console.log(err); // выведем ошибку в консоль
+      });
+    // -- Загрузка начальных данных //
+  }, [userName, userDescription, userAvatar, cards, userId]);
+
   return (
     <main className="main">
       <section className="profile">
@@ -102,32 +78,9 @@ function Main(props) {
         </button>
       </section>
       <ul className="elements">
-        <template id="card-template">
-          <li className="elements__element">
-            <img
-              src="https://picsum.photos/282?random=1"
-              alt="фото загруженное пользователем"
-              className="elements__image"
-            />
-            <div className="elements__captions">
-              <h2 className="elements__name">Карачаевск</h2>
-              <div className="elements__like-container">
-                <button
-                  className="elements__like-button"
-                  type="button"
-                  aria-label="отметить карточку"
-                ></button>
-                <span className="elements__like-counter">0</span>
-              </div>
-            </div>
-            <img
-              src="<%=require('./images/trash.svg')%>"
-              alt="корзина"
-              className="elements__trash"
-              aria-label="удалить карточку"
-            />
-          </li>
-        </template>
+        {cards.map((card) => (
+          <Card key={card._id} userId={userId} {...card} />
+        ))}
       </ul>
     </main>
   );
