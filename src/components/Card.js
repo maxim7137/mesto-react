5508ed128aa9dec75275acee493303bf1ad3edf6
maxim713 <maxim713@mail.ru>
2022-10-13 20:49:02 +0300

    feature: Вынесите компонент Card убрать лишние переменные

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import trash from '../images/trash.svg';
 const srcError =
   'https://camo.githubusercontent.com/2515d63ed9f010c45188fb16aa813f67c886fcb713f8395964abcbd22bd791ef/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f41394563427a64367438445a652f67697068792e676966';
-const buttonLikedStyle = {};
+
 // проверка своих карточек
 // лайкнута ли мной
 function isLikedByMe(array, userId) {
@@ -22,8 +22,6 @@ function errorHandler(event) {
 }
 
 function Card({ link, name, likes, userId, owner }) {
-  // owner._id | userId
-
   return (
     <li className="elements__element">
       <img
